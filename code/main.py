10a795262284b679c407b5d626f686d1fcd0a61d
s1nbo <77055060+s1nbo@users.noreply.github.com>
2025-05-11 20:04:19 +0200

    Removed Custom Part

--- code/main.py
+++ code/main.py
@@ -3,6 +3,6 @@ from model import Model
 self = Model()
 
 self.analyse_data()
-self.train_custom_model()
-#self.train_auto_model()
+#self.train_custom_model()
+self.train_auto_model()
 self.evaluate_model()
