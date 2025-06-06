531383bd92aba559b3529a10dc9aa5f67ab42727
s1nbo <77055060+s1nbo@users.noreply.github.com>
2025-06-06 12:24:22 +0200

    We now only call the class once

--- code/main.py
+++ code/main.py
@@ -2,14 +2,14 @@ from model import Model
 import wandb
 import yaml
 
-def sweep_entrypoint():
-    model = Model()
+def sweep_entrypoint(model: Model):
     model.run_sweep_training(wandb.config)
 
 if __name__ == "__main__":
     sweep_config = yaml.safe_load(open("sweep.yaml"))
     sweep_id = wandb.sweep(sweep_config, project="sustaineval")
-    wandb.agent(sweep_id, function=sweep_entrypoint, count=50)
+    m = Model()
+    wandb.agent(sweep_id, function=sweep_entrypoint(m), count=50)
 
 
 
