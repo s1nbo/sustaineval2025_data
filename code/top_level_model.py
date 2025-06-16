097f45ae5d524cbd1f14ca1b49b0c3ae79740ad4
s1nbo <77055060+s1nbo@users.noreply.github.com>
2025-06-16 16:46:34 +0200

    Functions Headers for top level model

--- code/top_level_model.py
+++ code/top_level_model.py
@@ -1,6 +1,6 @@
 from model import Model
 
-class TopLevel(Model):
+class SuperLabel(Model):
     def __init__(self):
         self.setup()
 
@@ -20,9 +20,31 @@ class TopLevel(Model):
         }
 
         self.load_data(top_class=True)
+    
+    def sort_data(self):
+        # split the data based on the task_a_label in self.validation['task_a_label']
+        l1, l2, l3, l4 = 0,0,0,0
+        return l1, l2, l3, l4 
+
+
+
+
+class SingleLabel(Model):
+    def __init__(self, super_label: int, data):
+        pass
+
+    def load_data(self):
+        # load only the data by given by the super model
+        pass
+
+    def generate_submission(self):
+        pass
+
+    # Maybe the rest can be used from Parent class?
+
 
 
 if __name__ == '__main__':
-    model = TopLevel()
-    model.optuna_training(n_trials=100, wandb_project='top_level')
-
+    model = SuperLabel()
+    model.train_model()
+    model.evaluate_model()
